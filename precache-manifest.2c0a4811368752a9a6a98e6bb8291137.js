c925eb83f403eb2f2796ea01af9e1421694d7560
dsolutions30 <65696006+dsolutions30@users.noreply.github.com>
2020-05-21 05:00:26 +0100

    Updates

---
+++ precache-manifest.2c0a4811368752a9a6a98e6bb8291137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8168dafb4be9e1a06525",
+    "url": "/my-geate-repo/static/css/main.a9f93dc8.chunk.css"
+  },
+  {
+    "revision": "8168dafb4be9e1a06525",
+    "url": "/my-geate-repo/static/js/main.8168dafb.chunk.js"
+  },
+  {
+    "revision": "2854228e6ba6c6edb52e",
+    "url": "/my-geate-repo/static/css/1.44981def.chunk.css"
+  },
+  {
+    "revision": "2854228e6ba6c6edb52e",
+    "url": "/my-geate-repo/static/js/1.2854228e.chunk.js"
+  },
+  {
+    "revision": "83dd2a9b41acd96edfa4",
+    "url": "/my-geate-repo/static/js/runtime~main.83dd2a9b.js"
+  },
+  {
+    "revision": "97a213c2b99938ccbbdcf28d32dccb7d",
+    "url": "/my-geate-repo/index.html"
+  }
+];
